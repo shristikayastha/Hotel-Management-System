849be256df7b43b57fc0ef7e24fc21fa2426ccc6
shristikayastha <159998814+shristikayastha@users.noreply.github.com>
2024-02-14 17:07:38 +0545

    Add files via upload

---
+++ hotel_management_system.cpp
@@ -0,0 +1,207 @@
+#include<iostream>
+using namespace std;
+class Hotel{
+private:
+struct  Node{
+int id,date;
+string name,roomtype;
+Node*next;
+};
+public:
+Node*head=NULL;
+void insert();
+void menu();
+void update();
+void search();
+void del();
+void sort();
+void show();
+};
+void Hotel::menu(){
+int choice;
+cout<<"\n"<<endl;
+cout<<"\t------------WELCOME TO HOTEL MANAGEMENT SYSTEM-----------\n "<<endl;
+cout<<"\n\t------------------HOTEL MANAGEMENT SYSTEM------------------";
+cout<<"\n\n S.NO  FUNCTIONS          DESCRIPTION     "<<endl;
+cout<<"\n1\t\tAllocate Room\t\tInsert New Room";
+cout<<"\n2\t\tSearch Room\t\t\tSearch Room with Room Id";
+cout<<"\n3\t\tUpdate Room\t\t\tUpdate  Room Record";
+cout<<"\n4\t\tDelete Room\t\t\t Delete Room with  Room Id";
+cout<<"\n5\t\tShow  Room Record\tShow Room Records that(we added)";
+cout<<"\n6\t\tExit"<<endl;
+cout<<"Enter your choice:"<<endl;
+cin>>choice;
+switch(choice){
+case 1:
+insert();
+menu();
+case 2:
+search();
+menu();
+case 3:
+update();
+menu();
+case 4:
+del();
+menu();
+case 5:
+sort();
+show();
+menu();
+default:
+cout<<"INVALID";
+}
+}
+void Hotel::insert(){
+cout<<"\n\t---------------HOTEL MANAGEMENT SYSTEM--------------\n";
+Node*newNode=new Node;
+cout<<"Enter Room ID:"<<endl;
+cin>>newNode->id;
+cout<<"Enter Customer Name:"<<endl;
+cin>>newNode->name;
+cout<<"Enter Allocated Date:"<<endl;
+cin>>newNode->date;
+cout<<"Enter Room Type(single/double/twin):"<<endl;
+cin>>newNode->roomtype;
+
+if(head==NULL){
+head=newNode;
+}
+else{
+Node*ptr=head;
+while(ptr->next!=NULL){
+ptr=ptr->next;
+}
+ptr->next= newNode;
+ }
+cout<<"\n\n\t\t New  Room Inserted";
+}
+void Hotel::search(){
+cout<<"\n\t---------------------HOTEL MANAGEMENT SYSTEM------------------\n";
+int  t_id;
+if(head==NULL){
+cout<<"\n\n Linked list is Empty";
+}
+else{
+cout<<"\n\nRoom ID:";
+cin>>t_id;
+Node*ptr=head;
+while(ptr!=NULL){
+if(t_id==ptr->id){
+cout<<"\n\nRoom ID:"<<ptr->id;
+cout<<"\n\nCustomer Name:"<<ptr->name;
+cout<<"\n\nRoom Allocated Date:"<<ptr->date;
+cout<<"\n\nRoom Type:"<<ptr->roomtype;
+}
+ptr=ptr->next;
+}
+}
+}
+void Hotel::update(){
+cout<<"\n\t---------------------HOTEL MANAGEMENT SYSTEM------------------\n"<<endl;
+int  t_id;
+if(head==NULL){
+cout<<"\n\n Linked list is Empty";
+}
+else{
+cout<<"\n\n Room ID";
+cin>>t_id;
+Node*ptr=head;
+while(ptr!=NULL){
+if(t_id==ptr->id){
+cout<<"\nRoom ID";
+cin>>ptr->id;
+cout<<"\nCustomer Name";
+cin>>ptr->name;
+cout<<"\nAllocated Date";
+cin>>ptr->date;
+cout<<"\nRoom Type";
+cin>>ptr->roomtype;
+cout<<"\n\n\t\t Updated Record Sucessfully";
+}
+ptr=ptr->next;
+}
+}
+}
+void Hotel::del(){
+cout<<"\n\t---------------------HOTEL MANAGEMENT SYSTEM------------------\n";
+int  t_id;
+if(head==NULL){
+cout<<"\n\n Linked list is Empty";
+}
+else{
+cout<<"\n\n Room ID";
+cin>>t_id;
+}
+if(t_id==head->id){
+Node*ptr=head;
+head=head->next;
+delete ptr;
+cout<<"Deleted Room Record Sucessfully\n";
+}
+else{
+Node*pre=head;
+Node*ptr=head;
+while(ptr!=NULL){
+if(t_id==ptr->id){
+pre->next=ptr->next;
+delete ptr;
+cout<<"Deleted  Room Record Sucessfully";
+break;
+}
+pre=ptr;
+ptr=ptr->next;
+}
+}
+}
+void Hotel::show(){
+Node*ptr=head;
+while(ptr!=NULL){
+cout<<"\n\n Room ID:"<<ptr->id;
+cout<<"\n\n Customer  Name:"<<ptr->name;
+cout<<"\n\n Allocated Date:"<<ptr->date;
+cout<<"\n\n Room Type:"<<ptr->roomtype;
+ptr->next;
+}
+}
+void Hotel::sort(){
+if(head==NULL){
+cout<<"\n\n Linked list is Empty";
+menu();
+}
+int count=0,t_date,t_id;
+string t_name,t_roomtype;
+Node*ptr=head;
+while(ptr!=NULL){
+count++;
+ptr=ptr->next;
+}
+for(int i=1;i<=count;i++){
+Node*ptr=head;
+for(int j=1;j<count;j++){
+if(ptr->id>ptr->next->id){
+t_id=ptr->id;
+t_name=ptr->name;
+t_date=ptr->date;
+t_roomtype=ptr->roomtype;
+//save data to current node
+ptr->id=ptr->next->id;
+ptr->name=ptr->next->name;
+ptr->date=ptr->next->date;
+ptr->roomtype=ptr->next->roomtype;
+//save data into next node
+ptr->next->id=t_id;
+ptr->next->name=t_name;
+ ptr->next->date=t_date;
+ ptr->next->roomtype=t_roomtype;
+}
+ptr=ptr->next;
+}
+}
+}
+int main()
+{
+Hotel  h;
+h.menu();
+return 0;
+}
